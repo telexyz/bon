6652f33be829917119049d1b04866d71aa1f0047
Tien Dung <dungtn@gmail.com>
2022-07-28 20:28:03 +0700

    result ok

--- src/main.zig
+++ src/main.zig
@@ -52,9 +52,9 @@ pub fn main() void {
     const lo_nibbles: v.u8x16 = simd.mm_and_si128(input, simd.mm_set1_epi8(0b0000_1111));
     const hi_nibbles: v.u8x16 = simd.mm_and_si128(simd.mm_srli_epi16(input, 4), simd.mm_set1_epi8(0b0000_1111));
     std.debug.print("\nlo_nibbles {x}\n", .{std.fmt.fmtSliceHexLower(&@as([16]u8, lo_nibbles))});
-    std.debug.print("lo_nibbles 06000101000d0d01060d060101010d00\n", .{});
+    std.debug.print("           06000101000d0d01060d060101010d00\n", .{});
     std.debug.print("\nhi_nibbles {x}\n", .{std.fmt.fmtSliceHexLower(&@as([16]u8, hi_nibbles))});
-    std.debug.print("hi_nibbles 03010902010e0e02030b030209090e01\n", .{});
+    std.debug.print("           03010902010e0e02030b030209090e01\n", .{});
 
     // row_0_07       = [a1|43|6f|6f|43|b8|b8|6f|a1|b8|a1|6f|6f|6f|b8|43]
     // row_8_15       = [14|24|b0|b0|24|0c|0c|b0|14|0c|14|b0|b0|b0|0c|24]
@@ -62,29 +62,34 @@ pub fn main() void {
     const row_8_15: v.u8x16 = simd.mm_shuffle_epi8(bitmap_8_15, lo_nibbles);
 
     std.debug.print("\nrow_0_07 {s}\n", .{std.fmt.fmtSliceHexLower(&@as([16]u8, row_0_07))});
-    std.debug.print("row_0_07 a1436f6f43b8b86fa1b8a16f6f6fb843\n", .{});
+    std.debug.print("         a1436f6f43b8b86fa1b8a16f6f6fb843\n", .{});
     std.debug.print("\nrow_8_15 {s}\n", .{std.fmt.fmtSliceHexLower(&@as([16]u8, row_8_15))});
-    std.debug.print("row_8_15 1424b0b0240c0cb0140c14b0b0b00c24\n", .{});
+    std.debug.print("         1424b0b0240c0cb0140c14b0b0b00c24\n", .{});
 
-    const bitmask_lookup: v.u8x16 = simd.mm_setr_epi8(1, 2, 4, 8, 16, 32, 64, 128, 1, 2, 4, 8, 16, 32, 64, 128);
-    std.debug.print("\nbitmask_lookup {d}\n", .{@as([16]u8, bitmask_lookup)});
+    const lookup: v.u8x16 = simd.mm_setr_epi8(1, 2, 4, 8, 16, 32, 64, 128, 1, 2, 4, 8, 16, 32, 64, 128);
+    std.debug.print("\nlookup  {x}\n", .{std.fmt.fmtSliceHexLower(&@as([16]u8, lookup))});
 
-    // bitmask_lookup { 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000 }
-    // hi_nibbles = [03|01|09|02|01|0e|0e|02|03|0b|03|02|09|09|0e|01]
-    // bitmask    = [08|02|02|02|02|40|40|02|08|08|08|04|02|02|40|02]
-    const bitmask: v.u8x16 = simd.mm_shuffle_epi8(bitmask_lookup, hi_nibbles);
+    // lookup       = [01|02|04|08|10|20|40|80|01|02|04|08|10|20|40|80]
+    // hi_nibbles   = [03|01|09|02|01|0e|0e|02|03|0b|03|02|09|09|0e|01]
+    // bitmask      = [08|02|02|02|02|40|40|02|08|08|08|04|02|02|40|02]
+    //                          ^^
+    // TODO: bitmask đang bị lệch ở byte thứ 4
+    const bitmask: v.u8x16 = simd.mm_shuffle_epi8(lookup, hi_nibbles);
     std.debug.print("bitmask {s}\n", .{std.fmt.fmtSliceHexLower(&@as([16]u8, bitmask))});
-    std.debug.print("bitmask 08020202024040020808080402024002\n", .{});
+    std.debug.print("        08020202024040020808080402024002\n", .{});
 
-    // mask           = [ff|ff|00|ff|ff|00|00|ff|ff|00|ff|ff|00|00|00|ff]
     const mask: v.u8x16 = simd.mm_cmplt_epi8(hi_nibbles, simd.mm_set1_epi8(8));
-    const bitsets: v.u8x16 = simd.mm_blendv_epi8(row_0_07, row_8_15, mask);
-
     std.debug.print("\nmask {s}\n", .{std.fmt.fmtSliceHexLower(&@as([16]u8, mask))});
-    std.debug.print("mask ffff00ffff0000ffff00ffff000000ff\n", .{});
-    std.debug.print("\nbitsets {s}\n", .{std.fmt.fmtSliceHexLower(&@as([16]u8, bitsets))});
+    std.debug.print("     ffff00ffff0000ffff00ffff000000ff\n", .{});
 
-    const tmp: v.u8x16 = simd.mm_and_si128(bitsets, bitmask);
+    const bitset: v.u8x16 = simd.mm_blendv_epi8(row_8_15, row_0_07, mask);
+    //          = [ff|ff|00|ff|ff|00|00|ff|ff|00|ff|ff|00|00|00|ff]
+    // row_0_07 ? [a1|43|..|6f|43|..|..|6f|a1|..|a1|6f|..|..|..|43]
+    // row_8_15 : [..|..|b0|..|..|0c|0c|..|..|0c|..|..|b0|b0|0c|..]
+    std.debug.print("\nbitset {s}\n", .{std.fmt.fmtSliceHexLower(&@as([16]u8, bitset))});
+    std.debug.print("       a143b06f430c0c6fa10ca16fb0b00c43\n", .{});
+
+    const tmp: v.u8x16 = simd.mm_and_si128(bitset, bitmask);
     const result: v.u8x16 = simd.mm_cmpeq_epi8(tmp, bitmask);
     // input          = [36|10|91|21|10|ed|ed|21|36|bd|36|21|91|91|ed|10]
     //                      ^^    ^^ ^^       ^^    ^^    ^^          ^^
