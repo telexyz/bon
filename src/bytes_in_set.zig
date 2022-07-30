26d10549e646701a735619b559c4584f3ddc2375
Tien Dung <dungtn@gmail.com>
2022-07-30 07:35:35 +0700

    use zig built-in @splat to fill-up vector

--- src/bytes_in_set.zig
+++ src/bytes_in_set.zig
@@ -6,7 +6,7 @@ const simd = @import("simd.zig");
 const fmtHex = std.fmt.fmtSliceHexLower;
 
 pub fn inSet(input_bytes: v.u8x16, bitmap_0_07: v.u8x16, bitmap_8_15: v.u8x16) v.u8x16 {
-    const lower = simd.setall8_m128(0b0000_1111);
+    const lower = @splat(16, @as(u8, 0b0000_1111));
     const lo_nibbles: v.u8x16 = simd.and_m128(input_bytes, lower);
     const hi_nibbles: v.u8x16 = simd.and_m128(simd.rshift16_m128(input_bytes, 4), lower);
     //                                          ^^ right shift cùng lúc 2 input_bytes
@@ -23,7 +23,7 @@ pub fn inSet(input_bytes: v.u8x16, bitmap_0_07: v.u8x16, bitmap_8_15: v.u8x16) v
 
     // Nếu hi_nibbles[i] mà <  8 thì bitsets[i] = row_0_07[i]
     // Nếu hi_nibbles[i] mà >= 8 thì bitsets[i] = row_8_15[i]
-    const mask: v.u8x16 = simd.cmplt8_m128(hi_nibbles, simd.setall8_m128(8));
+    const mask: v.u8x16 = simd.cmplt8_m128(hi_nibbles, @splat(16, @as(u8, 8)));
     const bitsets: v.u8x16 = simd.blend8_m128(row_8_15, row_0_07, mask);
 
     const results: v.u8x16 = simd.and_m128(bitsets, bitmasks);
@@ -34,15 +34,13 @@ pub fn inSetNoBlend(input_bytes: v.u8x16, bitmap_0_07: v.u8x16, bitmap_8_15: v.u
     // http://0x80.pl/articles/simd-byte-lookup.html#alternative-implementation-new
     // https://github.com/intel/hyperscan/blob/master/src/nfa/truffle.c
     //
-    const lower = simd.setall8_m128(0b0000_1111);
+    const lower = @splat(16, @as(u8, 0b0000_1111));
 
     const lo_nibbles = simd.and_m128(input_bytes, lower);
     const hi_nibbles = simd.and_m128(simd.rshift16_m128(input_bytes, 4), lower);
 
-    const msblo_nibbles = simd.and_m128(
-        input_bytes,
-        simd.setall8_m128(0b1000_1111),
-    ); //                   ^    ^^^^ keep the most significant bit (msb) and lo_nibbles
+    const msblo_nibbles = simd.and_m128(input_bytes, @splat(16, @as(u8, 0b1000_1111)));
+    // keep the most significant bit (msb) and lo_nibbles                 ^____^^^^
 
     const bitmasks = simd.pshufb_m128(
         simd.setrev8_m128(1, 2, 4, 8, 16, 32, 64, 128, 1, 2, 4, 8, 16, 32, 64, 128),
