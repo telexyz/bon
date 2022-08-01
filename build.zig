e1af9afbd6ba4ee0ae02d8e27db391dd8bcaeefd
Tien Dung <dungtn@gmail.com>
2022-08-01 16:27:34 +0700

    phân tích utf-8 code

--- build.zig
+++ build.zig
@@ -14,7 +14,8 @@ pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
 
     // const exe = b.addExecutable("bytes_in_set", "src/bytes_in_set.zig");
-    const exe = b.addExecutable("am_dau", "src/am_dau.zig");
+    // const exe = b.addExecutable("am_dau", "src/am_dau.zig");
+    const exe = b.addExecutable("am_tiet", "src/am_tiet.zig");
     setup(exe, mode, target);
     exe.install();
 
