fdf28539e8848fa4106c3264383b6e74a802297d
Tien Dung <dungtn@gmail.com>
2022-08-02 16:26:00 +0700

    save l3c (last three chars)

--- src/char_stream.zig
+++ src/char_stream.zig
@@ -1,6 +1,9 @@
 // - tìm vị trí space
 
 const std = @import("std");
+const v = @import("vector_types.zig");
+// const simd = @import("simd.zig");
+const BYTES_PROCESSED = 32;
 
 pub fn main() !void {
     var file = try std.fs.cwd().openFile("utf8tv.txt", .{});
@@ -9,11 +12,24 @@ pub fn main() !void {
     var buf_reader = std.io.bufferedReader(file.reader());
     var in_stream = buf_reader.reader();
 
-    var buf: [32]u8 = undefined;
+    var l3c: [3]u8 = undefined;
+    std.mem.set(u8, l3c[0..], 32);
+
+    var buf: [BYTES_PROCESSED]u8 = undefined;
+    var vec: v.u8x32 = undefined;
     var len = try in_stream.read(&buf);
 
     while (len > 0) {
-        std.debug.print("{s}\n", .{buf[0..len]});
+        const str = buf[0..len];
+        std.debug.print("{s}{s}\n", .{ l3c[0..], str });
+
+        vec = buf;
+        const sp_comp = vec == @splat(BYTES_PROCESSED, @as(u8, ' '));
+        const sp_bits = @ptrCast(*const u32, &sp_comp).*;
+        const sp_1st_idx = @ctz(u32, sp_bits);
+        std.debug.print("{b:0>31}\n{d}\n", .{ sp_bits, sp_1st_idx });
+
+        l3c = buf[BYTES_PROCESSED - 3 .. BYTES_PROCESSED].*;
         len = try in_stream.read(&buf);
     }
 }
