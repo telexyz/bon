3fab2cd5cfdba3840cc3f1d5629f2889fe8864ea
Tien Dung <dungtn@gmail.com>
2022-08-04 21:03:05 +0700

    print tone

--- src/am_tiet.zig
+++ src/am_tiet.zig
@@ -3,9 +3,11 @@ const sds = @import("syllable.zig"); // sds: Syllable Data Structures
 const getInitial = @import("am_dau.zig").getInitial;
 const getMiddle = @import("am_giua.zig").getMiddle;
 
+const MAX_SYLLABLE_LEN = 10;
+
 pub fn parseSyllable(str: []const u8) sds.Syllable {
     var syll = sds.Syllable.new();
-    if (str.len > 10) return syll;
+    if (str.len > MAX_SYLLABLE_LEN) return syll;
 
     var c0: Char = undefined;
     var c1: Char = undefined;
@@ -13,26 +15,24 @@ pub fn parseSyllable(str: []const u8) sds.Syllable {
     c0.parse(str, 0);
     var pos = c0.len();
 
-    var initial: sds.AmDau = undefined;
-
     if (str.len > 1) {
         // chỉ phân tích âm đầu khi có 2 ký tự trở lên
         // vì âm tiết lúc nào cũng có nguyên âm
 
         if (pos > 1) { // đ
-            initial = getInitial(c0.byte1, c0.byte0);
+            syll.am_dau = getInitial(c0.byte1, c0.byte0);
         } else {
             c1.parse(str, pos);
             pos += c1.len();
-            initial = getInitial(c0.byte0, c1.byte0);
+            syll.am_dau = getInitial(c0.byte0, c1.byte0);
         }
     }
 
     // bỏ qua h của ngh
-    if (initial == .ng and (str[pos] == 'h' or str[pos] == 'H')) pos += 1;
+    if (syll.am_dau == .ng and (str[pos] == 'h' or str[pos] == 'H')) pos += 1;
 
     // phân tích âm giữa
-    if (initial.len() == 2) {
+    if (syll.am_dau.len() == 2) {
         c0.parse(str, pos);
         pos += c0.len();
         c1.parse(str, pos);
@@ -54,11 +54,15 @@ pub fn parseSyllable(str: []const u8) sds.Syllable {
         c1.parse(str, pos);
     }
 
-    const middle = getMiddle(c0.byte0, c0.byte1, c1.byte0, c1.byte1);
+    syll.am_giua = getMiddle(c0.byte0, c0.byte1, c1.byte0, c1.byte1);
+
+    // xác định thanh điệu
+    syll.tone = c0.tone;
+    if (syll.tone == ._none) syll.tone = c1.tone;
 
     std.debug.print(
-        "\n{s: >11}: {s: >5} {s: >5}",
-        .{ str, @tagName(initial), @tagName(middle) },
+        "\n{s: >11}: {s: >5} {s: >5} {s: >5} {s: >5}",
+        .{ str, @tagName(syll.am_dau), @tagName(syll.am_giua), @tagName(syll.am_cuoi), @tagName(syll.tone) },
     );
 
     return syll;
@@ -68,12 +72,12 @@ const Char = struct {
     byte0: u8 = undefined,
     byte1: u8 = undefined,
     isUpper: bool = undefined,
-    tone: u8 = undefined,
+    tone: sds.Tone = undefined,
 
     pub inline fn parse(self: *Char, str: []const u8, idx: usize) void {
-        self.tone = 0;
-
         const x = str[idx];
+        self.tone = ._none;
+
         switch (x) {
             0...127 => {
                 // std.debug.print("\n\n{c}: {x}", .{ x, x });
@@ -96,60 +100,60 @@ const Char = struct {
                 switch (y) {
                     160 => {
                         self.byte0 = 'a';
-                        self.tone = 'f';
+                        self.tone = .f;
                     },
                     161 => {
                         self.byte0 = 'a';
-                        self.tone = 's';
+                        self.tone = .s;
                     },
                     163 => {
                         self.byte0 = 'a';
-                        self.tone = 'x';
+                        self.tone = .x;
                     },
                     168 => {
                         self.byte0 = 'e';
-                        self.tone = 'f';
+                        self.tone = .f;
                     },
                     169 => {
                         self.byte0 = 'e';
-                        self.tone = 's';
+                        self.tone = .s;
                     },
                     172 => {
                         self.byte0 = 'i';
-                        self.tone = 'f';
+                        self.tone = .f;
                     },
                     173 => {
                         self.byte0 = 'i';
-                        self.tone = 's';
+                        self.tone = .s;
                     },
                     178 => {
                         self.byte0 = 'o';
-                        self.tone = 'f';
+                        self.tone = .f;
                     },
                     179 => {
                         self.byte0 = 'o';
-                        self.tone = 's';
+                        self.tone = .s;
                     },
                     181 => {
                         self.byte0 = 'o';
-                        self.tone = 'x';
+                        self.tone = .x;
                     },
                     185 => {
                         self.byte0 = 'u';
-                        self.tone = 'f';
+                        self.tone = .f;
                     },
                     186 => {
                         self.byte0 = 'u';
-                        self.tone = 's';
+                        self.tone = .s;
                     },
                     189 => {
                         self.byte0 = 'y';
-                        self.tone = 's';
+                        self.tone = .s;
                     },
                     else => {
                         self.byte0 = y;
                         self.byte1 = x;
-                        self.tone = 0;
+                        self.tone = ._none;
                     },
                 }
             },
@@ -180,8 +184,10 @@ const Char = struct {
 };
 
 pub fn main() void {
+    std.debug.print("\n{s: >11}: {s: >5} {s: >5} {s: >5} {s: >5}\n     - - - - - - - - - - - - - - - -", .{ "ÂM TIẾT", "ĐẦU", "GIỮA", "CUỐI", "THANH" });
     _ = parseSyllable("GÀN");
     _ = parseSyllable("GáN");
+    _ = parseSyllable("GIúp");
     _ = parseSyllable("nGhiÊng");
     _ = parseSyllable("nGiêng");
 
