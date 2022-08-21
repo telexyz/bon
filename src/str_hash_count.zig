db64571136308ab27887cafc457a2793548246d8
Tien Dung <dungtn@gmail.com>
2022-08-22 06:57:10 +0700

    src/str_hash_count.zig: small string có len <= 8 sử dụng hash để lưu key luôn

--- src/str_hash_count.zig
+++ src/str_hash_count.zig
@@ -4,8 +4,8 @@
 // Mỗi hashtable entry gồm:
 // * `hash` u64
 // * `count` là u24
-// * `offset` u48, trỏ tới vị trí đầu của key trong keys_bytes hoặc lưu 1 cặp u24,u24 dùng trong BPE
-// => Total 16-bytes (25% cache-line)
+// * `offset` u24, trỏ tới vị trí đầu của key trong keys_bytes nếu key là string
+// => Total 14-bytes (22% cache-line)
 //
 // HashCount chỉ cần 2 thao tác là `insert` và `count`
 // HashCount cho phép nhiều threads truy cập
@@ -21,13 +21,16 @@
 //
 // Có 2 cách cài đặt hash map tốt là `libs/youtokentome/third_party/flat_hash_map.h` và
 // `cswisstable`; có thể tìm hiểu cả 2 để có lựa chọn tốt nhất cho HashCount.
+//
+// >> small string type count: 1_099_201, ss bytes: 6_788_771, remain: 14_427_001 <<
 
 const std = @import("std");
 const builtin = @import("builtin");
 
 pub const HashType = u64;
 pub const CountType = u24;
-pub const IndexType = u48; // Để lưu được 1 cặp u24,u24 (dùng trong BPE)
+pub const IndexType = u24;
+pub const PairType = u48;
 
 pub const GUARD_BYTE = 32; // vì token ko có space nên gán = 32 để in ra dễ đọc
 
@@ -53,11 +56,11 @@ pub fn HashCount(comptime cfg: Config) type {
     const bits = std.math.log2_int(u64, cfg.capacity);
     const shift = 63 - bits;
     const size = (@as(usize, 2) << bits) + cfg.capacity;
-    const KeyType = if (cfg.for_bpe) IndexType else []const u8;
+    const KeyType = if (cfg.for_bpe) PairType else []const u8;
 
     std.debug.assert(size < MAX_CAPACITY);
     std.debug.assert(size > cfg.capacity);
-    std.debug.assert(cfg.capacity * AVG_KEY_LEN < MAX_CAPACITY);
+    // std.debug.assert(cfg.capacity * AVG_KEY_LEN < MAX_CAPACITY);
 
     return struct {
         const lock_init = if (builtin.single_threaded) {} else false;
@@ -91,23 +94,20 @@ pub fn HashCount(comptime cfg: Config) type {
             self.total_puts = 0;
 
             self.len = 0;
-            self.keys_bytes_len = 0;
+            self.keys_bytes_len = 9; // 0-8 để dành lưu len của small string
 
             self.spinlock = lock_init;
             self.allocator = init_allocator;
 
             if (!cfg.for_bpe) {
-                self.keys_bytes = try self.allocator.alloc(u8, cfg.capacity * AVG_KEY_LEN);
+                var n: usize = cfg.capacity * AVG_KEY_LEN;
+                if (n > std.math.maxInt(IndexType)) n = std.math.maxInt(IndexType);
+                self.keys_bytes = try self.allocator.alloc(u8, n);
                 std.mem.set(u8, self.keys_bytes, GUARD_BYTE);
             }
 
             self.entries = try self.allocator.alloc(Entry, size);
-            std.mem.set(Entry, self.entries, .{ .hash = maxx_hash, .count = 0, .offset = maxx_index });
-        }
-
-        pub fn keyStr(self: *Self, offset: IndexType) []const u8 {
-            const ending: usize = offset + self.keys_bytes[offset - 1];
-            return self.keys_bytes[offset..ending];
+            std.mem.set(Entry, self.entries, .{ .hash = maxx_hash, .count = 0, .offset = 0 });
         }
 
         inline fn recordStats(self: *Self, _probs: usize) void {
@@ -117,11 +117,33 @@ pub fn HashCount(comptime cfg: Config) type {
             if (probs > self.max_probs) self.max_probs = probs;
         }
 
-        inline fn _hash(key: KeyType) u64 {
+        fn keyPair(entry: Entry) PairType {
+            return @intCast(PairType, entry.hash *% 0x2040003d780970bd);
+        }
+
+        fn keyStr(self: Self, entry: *const Entry, ss_ptr: *HashType) []const u8 {
+            const offset = entry.offset;
+            if (offset <= 8) { // small string
+                ss_ptr.* = entry.hash *% 0x2040003d780970bd;
+                return std.mem.asBytes(ss_ptr)[0..offset];
+            }
+            const ending: usize = offset + self.keys_bytes[offset - 1];
+            return self.keys_bytes[offset..ending];
+        }
+        // x == (x * 0x517cc1b727220a95) * 0x2040003d780970bd // wrapping_mul
+        inline fn _hash(key: KeyType) HashType {
             if (cfg.for_bpe) {
-                return std.hash.Wyhash.hash(3322, std.mem.asBytes(&key)[0..]);
+                return @intCast(HashType, key) *% 0x517cc1b727220a95;
             } else {
-                return std.hash.Wyhash.hash(key[0], key);
+                if (key.len <= 8) {
+                    var value: HashType = 0;
+                    for (key) |byte, i| {
+                        value += @intCast(HashType, byte) << @intCast(u6, i) * 8;
+                    }
+                    return value *% 0x517cc1b727220a95;
+                } else {
+                    return std.hash.Wyhash.hash(key[0], key);
+                }
             }
         }
 
@@ -143,33 +165,45 @@ pub fn HashCount(comptime cfg: Config) type {
                 }
             }
 
-            self.recordStats(i - _i);
-
             var entry = &self.entries[i];
-            const key_exists = (cfg.for_bpe and entry.offset == key) or entry.hash == it.hash;
-            if (key_exists) { // key đã tồn tại từ trước
-                entry.count += count; // xáo trộn duy nhất là thay đổi giá trị count
-                return i;
+            var ss: HashType = undefined;
+            const ss_ptr = &ss;
+
+            while (entry.hash == it.hash) {
+                const found = cfg.for_bpe or std.mem.eql(u8, self.keyStr(entry, ss_ptr), key);
+
+                if (found) { // key đã tồn tại từ trước
+                    entry.count += count; // xáo trộn duy nhất là thay đổi giá trị count
+                    return i;
+                }
+
+                i += 1;
+                entry = &self.entries[i];
             }
+
+            self.recordStats(i - _i);
 
             { // Chỉ dùng lock khi có xáo trộn dữ liệu lớn
                 while (@atomicRmw(bool, &self.spinlock, .Xchg, true, .SeqCst)) {}
                 defer std.debug.assert(@atomicRmw(bool, &self.spinlock, .Xchg, false, .SeqCst));
 
                 // key lần đầu xuất hiện, ghi lại offset
-                if (cfg.for_bpe) {
-                    it.offset = key;
-                } else {
-                    var ending = self.keys_bytes_len;
-                    self.keys_bytes[ending] = @intCast(u8, key.len);
-                    it.offset = @intCast(IndexType, ending + 1);
-                    ending += 1;
-                    for (key) |byte| {
-                        self.keys_bytes[ending] = byte;
+                // for_bpe key đã được mã hoá trong hash nên ko cần ghi lại
+                if (!cfg.for_bpe) {
+                    if (key.len <= 8) {
+                        it.offset = @intCast(IndexType, key.len);
+                    } else {
+                        var ending = self.keys_bytes_len;
+                        self.keys_bytes[ending] = @intCast(u8, key.len);
+                        it.offset = @intCast(IndexType, ending + 1);
                         ending += 1;
+                        for (key) |byte| {
+                            self.keys_bytes[ending] = byte;
+                            ending += 1;
+                        }
+                        self.keys_bytes[ending] = GUARD_BYTE;
+                        self.keys_bytes_len = ending + 1;
                     }
-                    self.keys_bytes[ending] = GUARD_BYTE;
-                    self.keys_bytes_len = ending + 1;
                 }
 
                 while (true) : (i += 1) {
@@ -180,7 +214,7 @@ pub fn HashCount(comptime cfg: Config) type {
                     // Tráo giá trị it và entries[i] để đảm bảo tính tăng dần của hash
                     const tmp = self.entries[i];
                     self.entries[i] = it;
-                    if (tmp.offset == maxx_index) { // ô rỗng, dừng thuật toán
+                    if (tmp.offset == 0) { // ô rỗng, dừng thuật toán
                         self.len += 1; // thêm 1 phần tử mới được ghi vào HashCount
                         return i;
                     }
@@ -190,39 +224,49 @@ pub fn HashCount(comptime cfg: Config) type {
         }
 
         pub fn get(self: *Self, key: KeyType) CountType {
-            const entry = self.get_entry(key);
+            const entry = self.getEntry(key);
             if (entry == null) return 0 else return entry.?.count;
         }
 
-        pub fn get_entry(self: *Self, key: KeyType) ?*Entry {
+        pub fn getEntry(self: *Self, key: KeyType) ?*Entry {
             if (!cfg.for_bpe and key.len > MAX_KEY_LEN) return null;
-
             const hash = _hash(key);
-
             var i = hash >> shift;
-            // Vì hash value luôn tăng nên khi entry.hash > hash nghĩa là key chưa dc đếm
-            while (true) : (i += 1) {
-                var entry = &self.entries[i];
-                if (entry.hash < hash) continue;
-                return if ((cfg.for_bpe and entry.offset == key) or entry.hash == hash)
-                    entry
-                else
-                    null;
+
+            while (self.entries[i].hash < hash) : (i += 1) {}
+
+            var entry = &self.entries[i];
+            var ss: HashType = undefined;
+            const ss_ptr = &ss;
+
+            while (entry.hash == hash) {
+                const found = cfg.for_bpe or std.mem.eql(u8, self.keyStr(entry, ss_ptr), key);
+                if (found) {
+                    return entry;
+                }
+
+                i += 1;
+                entry = &self.entries[i];
             }
+
+            return null;
         }
 
         pub fn validate(self: *Self) bool {
             var prev: HashType = 0;
-            for (self.entries[0..]) |entry| {
+            var ss: HashType = undefined;
+            const ss_ptr = &ss;
+
+            for (self.entries[0..]) |*entry| {
                 const curr = entry.hash;
                 if (curr < maxx_hash) {
-                    if (prev >= curr) {
+                    if (prev > curr) {
                         std.debug.print("\n!! hash ko tăng dần !!\n", .{});
                         return false;
                     }
                     prev = curr;
 
-                    if (curr != _hash(self.keyStr(entry.offset))) {
+                    if (curr != _hash(self.keyStr(entry, ss_ptr))) {
                         std.debug.print("\n!! hash ko trùng với key !!\n", .{});
 
                         return false;
@@ -261,7 +305,6 @@ pub const CountDesc = struct {
         self.allocator.free(self.vocabs);
     }
 
-    // const pext_u32 = @import("instructions.zig").pext_u32;
     pub fn init(self: *Self, allocator: std.mem.Allocator, len: usize, entries: []const Entry, keys_bytes: []const u8, keys_bytes_len: usize) !void {
         self.allocator = allocator;
         self.len = len;
@@ -271,31 +314,39 @@ pub const CountDesc = struct {
         std.mem.set(Entry, self.entries, .{ .hash = maxx_hash, .count = 0, .offset = maxx_index });
 
         var i: IndexType = 0;
+        var ss_puts: usize = 0;
+        var ss_count: usize = 0;
+        var ss_bytes: usize = 0;
         for (self.entries) |*new_entry| {
             while (entries[i].count == 0) : (i += 1) {} // bỏ qua
             new_entry.* = entries[i];
+            if (new_entry.offset <= 8) {
+                ss_puts += new_entry.count;
+                ss_count += 1;
+                ss_bytes += new_entry.offset;
+            }
             i += 1;
         }
+        std.debug.print("\n\n\n>> small string count: {d}, ss puts: {d}, ss bytes: {d}, remain: {d} <<\n", .{ ss_count, ss_puts, ss_bytes, keys_bytes_len });
         std.sort.sort(Entry, self.entries, self, count_desc);
 
-        self.vocabs = try self.allocator.alloc(u8, keys_bytes_len + len * 3);
+        self.vocabs = try self.allocator.alloc(u8, keys_bytes_len + len * 20);
         // cần thêm 3-bytes lưu count
 
         var x: usize = 0;
+        var ss: HashType = undefined;
+        const ss_ptr = &ss;
         for (self.entries) |entry| {
+            const key_str = self.keyStr(entry, ss_ptr);
+
             // 3-bytes đầu lưu count
             self.vocabs[x] = @intCast(u8, (entry.count >> 16) & 0x00ff);
             self.vocabs[x + 1] = @intCast(u8, (entry.count >> 8) & 0x00ff);
             self.vocabs[x + 2] = @intCast(u8, entry.count & 0x00ff);
-            x += 3;
-
-            const l = keys_bytes[entry.offset - 1];
-            const end = entry.offset + l;
-            self.vocabs[x] = l + 1; // key's len
-            x += 1;
-
+            self.vocabs[x + 3] = @intCast(u8, key_str.len) + 1; // key's len
+            x += 4;
             // copy key bytes
-            for (keys_bytes[entry.offset..end]) |byte| {
+            for (key_str) |byte| {
                 self.vocabs[x] = byte;
                 x += 1;
             }
@@ -304,6 +355,16 @@ pub const CountDesc = struct {
             x += 1;
         }
         self.vocabs_len = x;
+    }
+
+    pub fn keyStr(self: *Self, entry: Entry, ss_ptr: *HashType) []const u8 {
+        const offset = entry.offset;
+        if (offset <= 8) { // small string
+            ss_ptr.* = entry.hash *% 0x2040003d780970bd;
+            return std.mem.asBytes(ss_ptr)[0..offset];
+        }
+        const ending: usize = offset + self.keys_bytes[offset - 1];
+        return self.keys_bytes[offset..ending];
     }
 
     pub fn vocabs_slice(self: *Self) []const u8 {
@@ -316,13 +377,11 @@ pub const CountDesc = struct {
         const n = if (max < self.len) max else self.len;
         var x: usize = 0;
         while (i < n) : (i += 1) {
-            // count trích xuất từ 2-bytes đầu tiên
+            // count trích xuất từ 3-bytes đầu tiên
             const count = (@as(u24, self.vocabs[x]) << 16) + (@as(u24, self.vocabs[x + 1]) << 8) + self.vocabs[x + 2];
-            x += 3;
-
             // byte tiếp theo chứa key's len
-            const len = self.vocabs[x];
-            x += 1;
+            const len = self.vocabs[x + 3];
+            x += 4;
 
             // các bytes tiếp theo là của key
             const end = x + len;
@@ -335,16 +394,10 @@ pub const CountDesc = struct {
         }
     }
 
-    pub fn keyStr(self: Self, idx: usize) []const u8 {
-        const offset = self.entries[idx].offset;
-        var ending: usize = offset + self.keys_bytes[offset - 1];
-        return self.keys_bytes[offset..ending];
-    }
-
     fn count_desc(context: *Self, a: Entry, b: Entry) bool {
-        // _ = context;
-        // return a.count > b.count;
-        return context.keys_bytes[a.offset - 1] > context.keys_bytes[b.offset - 1];
+        const al = if (a.offset <= 8) a.offset else context.keys_bytes[a.offset - 1];
+        const bl = if (b.offset <= 8) b.offset else context.keys_bytes[b.offset - 1];
+        return al > bl;
     }
 };
 
