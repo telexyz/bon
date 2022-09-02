b8824655cb183c3202784154308d9ddd286c9371
Tien Dung <dungtn@gmail.com>
2022-09-02 07:46:58 +0700

    chuẩn bị làm text-editor tích hợp bộ gõ fingers

---
+++ src/fingers.zig
@@ -0,0 +1,4 @@
+// Terminal-based Text-editor để tích hợp bộ gõ song ngữ Anh - Việt thông minh,
+// tự cải thiện trong quá trình tương tác với người dùng.
+// https://viewsourcecode.org/snaptoken/kilo
+
